4474aa57f60cbc97b89c06ea2c533439b4cb1ade
EvaEwall <550808296@qq.com>
2020-05-22 11:37:37 +0800

    修改代码

---
+++ Example/PodTest/PTTest.m
@@ -0,0 +1,15 @@
+//
+//  PTTest.m
+//  PodTest_Example
+//
+//  Created by zhousuntai on 2020/5/22.
+//  Copyright © 2020 550808296@qq.com. All rights reserved.
+//
+
+#import "PTTest.h"
+
+@implementation PTTest
+- (void)showMe{
+    NSLog(@"哈哈哈哈哈哈哈哈");
+}
+@end
---
+++ PodTest/Classes/PTTest.h
@@ -0,0 +1,17 @@
+//
+//  PTTest.h
+//  PodTest_Example
+//
+//  Created by zhousuntai on 2020/5/22.
+//  Copyright © 2020 550808296@qq.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PTTest : NSObject
+- (void)showMe;
+@end
+
+NS_ASSUME_NONNULL_END
